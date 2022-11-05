c13bfbb8afed8d28f3aabb7436fce26867b89d64
K3rnelPan1c <69395733+k3rnelpan1c-dev@users.noreply.github.com>
2022-11-06 00:39:06 +0100

    add: initial server deb build workflow

---
+++ server/buildDebInContainer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -exu
+
+pushd jellyfin-web
+
+# workaround for manually installed dotnet
+sed -i -e '/dotnet-sdk-6.0,/d' debian/control
+
+# build deb
+export CONFIG_SITE="/etc/dpkg-cross/cross-config.${1:-amd64}"
+dpkg-buildpackage -us -uc --pre-clean --post-clean
+
+popd
+
+# move
+mkdir out
+mv jellyfin-web*.{deb,dsc,tar.gz,buildinfo,changes} out
