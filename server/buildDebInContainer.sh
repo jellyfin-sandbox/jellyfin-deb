32a60511552cf9fe25b94064ebb90d48253e26a7
K3rnelPan1c <69395733+k3rnelpan1c-dev@users.noreply.github.com>
2022-11-13 16:24:34 +0100

    add: server deb cross build

--- server/buildDebInContainer.sh
+++ server/buildDebInContainer.sh
@@ -8,8 +8,8 @@ pushd jellyfin-code
 sed -i -e '/dotnet-sdk-6.0,/d' debian/control
 
 # build deb
-export CONFIG_SITE="/etc/dpkg-cross/cross-config.${1:-amd64}"
-dpkg-buildpackage -us -uc --pre-clean --post-clean
+export CONFIG_SITE="/etc/dpkg-cross/cross-config.amd64"
+dpkg-buildpackage --unsigned-source --unsigned-changes --pre-clean --post-clean --host-arch "${1:-amd64}"
 
 popd
 
